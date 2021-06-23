0a7148a0cd49dc2d3ffca5699f637bafeab6b487
IanaCris <ianasousati@gmail.com>
2021-06-22 23:46:00 -0300

    create entity tag

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import { CreateUserService } from "../services/CreateUserService";
 
-
 class CreateUserController {
   async handle(request: Request, response: Response) {
     const { name, email, admin } = request.body;
