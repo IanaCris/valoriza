0c0eb7eb2be100349ddae0003fd85ee0ddffae22
IanaCris <ianasousati@gmail.com>
2021-06-26 16:33:23 -0300

    authenticate user

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,41 @@
+import { getCustomRepository } from "typeorm";
+
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+interface IAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+
+  async execute({email, password}: IAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    const user = await usersRepositories.findOne({ email });
+
+    if (!user) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) { 
+      throw new Error("Email/Password incorrect");
+    }
+
+    const token = sign({
+      email: user.email
+    }, "7c23acf93087acb13dadba623dfc2f77", {
+      subject: user.id,
+      expiresIn: "1d"
+    });
+
+    return token;
+  }
+}
+
+export { AuthenticateUserService };
