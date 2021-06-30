d24dae813bdd99036dde9b0a826bdd041a024b44
IanaCris <ianasousati@gmail.com>
2021-06-29 21:32:27 -0300

    list tags

--- src/entities/Tag.ts
+++ src/entities/Tag.ts
@@ -2,6 +2,8 @@ import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } fro
 
 import { v4 as uuid } from "uuid";
 
+import { Expose } from "class-transformer";
+
 @Entity("tags")
 class Tag {
   @PrimaryColumn()
@@ -15,6 +17,11 @@ class Tag {
 
   @UpdateDateColumn()
   updated_at: Date;
+
+  @Expose({ name: "name_custom" })
+  nameCustom(): string {
+    return `#${this.name}`;
+  }
 
   constructor() {
     if (!this.id) {
