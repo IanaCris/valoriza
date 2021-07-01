e9ce1231f19abfc9f219b4d644ee2dcf15100603
IanaCris <ianasousati@gmail.com>
2021-07-01 00:17:23 -0300

    init documentation with swagger

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
+import swaggerUi  from "swagger-ui-express";
 
 import { router } from "./routes";
 
 import "./database";
+import swaggerDocs from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(router);
 
